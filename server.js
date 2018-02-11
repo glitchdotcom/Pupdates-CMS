670a6f01a20650692f2475d0856a9e425862969f
Glitch (recurly-plans) <none>
2018-02-11 19:51:07 +0000

    🕋🤗 Checkpoint package.json:471426/9 server.js:471426/829 public/index.html:471426/65

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const bodyParser = require("body-parser")
 const express = require("express")
+const Recurly = require("recurly")
 
 const app = express()
+const recurly = new Recurly({
+  API_KEY: process.env.RECURLY_API_KEY,
+  SUBDOMAIN: "glitch",
+  ENVIRONMENT: "sandbox",
+  DEBUG: true
+})
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post("/form", (req, res) => {
-  res.json("OK")
+  console.log(req.body)
+  const details = {
+    plan_code: "pyrite",
+    currency: "USD",
+    account: {
+      account_code: "can_this_be_set",
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      
+    billing_info: {
+      token_id: req.body.recurly_token
+    }
+    }
+  }
+  recurly.subscriptions.create(details, (err, subscription) => {
+    if (err) {
+      console.error(err)
+      res.status(503).json(err)
+    } else {
+      res.json(subscription)
+    }
+  })
 })
 
 const listener = app.listen(process.env.PORT, function () {
