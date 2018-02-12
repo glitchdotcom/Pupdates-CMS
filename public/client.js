d6500a267c12e0bd4b173aa2f20e7f8b3d36bed0
Glitch (empty-welcome) <none>
2018-02-12 16:27:07 +0000

    🐔🍌 Checkpoint server.js:471426/14 public/index.html:471426/9 public/client.js:471426/93 .env:471426/3 package.json:471426/5 public/style.css:471426/84 README.md:471426/133

--- public/client.js
+++ public/client.js
@@ -1,20 +1,3 @@
-/* global recurly */
-
 window.addEventListener("load", (e) => {
   console.log("LOADED")
-  
-  recurly.configure("ewr1-p2q4CkseaWl7fKQMPGKtn0")
-  
-  const form = document.getElementById("recurly-form")
-  form.addEventListener("submit", (e) => {
-    console.log("SUBMITTED")
-    e.preventDefault()
-    recurly.token(form, (err, token) => {
-      if (err) {
-        console.log(err.code, err.fields)
-      } else {
-        form.submit()
-      }
-    })
-  })
 })
--- server.js
+++ server.js
@@ -1,41 +1,10 @@
 const bodyParser = require("body-parser")
 const express = require("express")
-const Recurly = require("recurly")
 
 const app = express()
-const recurly = new Recurly({
-  API_KEY: process.env.RECURLY_API_KEY,
-  SUBDOMAIN: "glitch",
-  ENVIRONMENT: "sandbox",
-  DEBUG: true
-})
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false}))
-
-app.post("/form", (req, res) => {
-  console.log(req.body)
-  const details = {
-    plan_code: "pyrite",
-    currency: "USD",
-    account: {
-      account_code: `${Math.random()}`,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      billing_info: {
-        token_id: req.body.recurly_token
-      }
-    }
-  }
-  recurly.subscriptions.create(details, (err, subscription) => {
-    if (err) {
-      console.error(err)
-      res.status(err.statusCode).json(err.data.errors)
-    } else {
-      res.json(subscription)
-    }
-  })
-})
 
 const listener = app.listen(process.env.PORT, function () {
   console.log(`Your app is listening on port ${listener.address().port}`)
