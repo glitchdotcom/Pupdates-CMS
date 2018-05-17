38ee2c23eb3166f4ea5a548d7d074b9421bc4748
Glitch (empty-welcome) <none>
2018-05-17 16:18:16 +0000

    🌚📺 Checkpoint ./server.js:728660/164

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require("express")
 
 const app = express()
 
+function wrap (fn) {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false}))
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, async () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
