858ecaa684d70c500367caa0e0e0800e4b5df74f
Glitch (parcel-test) <none>
2018-10-19 16:32:16 +0000

    👲🏤 Checkpoint ./src/frontend/index.js:8/2096:728660/1598 ./chat.txt:8/1160:728660/1274

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -6,21 +6,17 @@ import Button from '@material-ui/core/Button';
 let rg = [1,2,3,4];
 
 const App = () => (
-  <Button variant="contained" color="primary">
-    Hello World
-  </Button>
+  <div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <div onClick={()=>{}}>
+      This div should trigger an accessibility linting error, because you don't click on divs. On the client?
+    </div>
+                                              
+                                              
+  </div>
 );
-// nice, ha?
-// what is missing keys?
-// see web console: 
-/*Warning: Each child in an array or iterator should have a unique "key" prop.
 
-Check the render method of `App`. See https://fb.me/react-warning-keys for more information.
-    in span (created by App)
-    in App */
-// ohhh gotcha! Perhaps SSR might do it?
-// ah yeah, maybe
-// i wonder about accessibility...
-// I think I can shave 2 seconds by removing the development NODE_ENV.
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App/>, mountNode);
