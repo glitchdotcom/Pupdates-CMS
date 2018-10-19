94add8603c8b134a4224bd660a873f8891281a7b
Glitch (parcel-test) <none>
2018-10-19 22:20:22 +0000

    🚁❣️ Checkpoint ./tools/build.sh:728660/238 ./src/frontend/index.js:728660/2 ./src/frontend/components/circular-indicator.js:728660/1600

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# production: small bundle, no React webtools
+# development: larger bundle, source maps
+NODE_ENV=development
+
+echo "Building..."
 rm -rf dist/
 if [[ $NODE_ENV == "production" ]]; then
   parcel build --no-autoinstall src/frontend/index.html
 else
-  export NODE_ENV="development"
   parcel build --no-minify --no-autoinstall src/frontend/index.html
 fi
+echo "done!"
