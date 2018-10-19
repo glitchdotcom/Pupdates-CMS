23ca95fe353cd181fc6d89f6831414cec432fd77
Glitch (parcel-test) <none>
2018-10-19 16:21:26 +0000

    🐀🌌 Checkpoint ./src/frontend/index.js:8/682:728660/169 ./chat.txt:728660/60

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 
 import Button from '@material-ui/core/Button';
 
+let rg = [1,2,3,4];
+
 const App = () => (
   <Button variant="contained" color="primary">
     Hello World
   </Button>
 );
+// nice, ha?
+// what is missing keys?
+// see web console: 
+/*Warning: Each child in an array or iterator should have a unique "key" prop.
 
+Check the render method of `App`. See https://fb.me/react-warning-keys for more information.
+    in span (created by App)
+    in App */
+// ohhh gotcha! Perhaps SSR might do it?
+// ah yeah, maybe
+// i wonder about accessibility...
+// I think I can shave 2 seconds by removing the development NODE_ENV.
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App/>, mountNode);
