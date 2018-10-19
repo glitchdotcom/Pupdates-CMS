228e8d605b543cb23c903995be8936ea7e2f83e9
Glitch (parcel-test) <none>
2018-10-19 17:35:00 +0000

    🎪😾 Checkpoint ./src/frontend/index.js:728660/250

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <CircularProgress />
+    </div>
   );
 }
 
