8e572c3261da393f2d8b97c90d70dd2f793701cc
Glitch (parcel-test) <none>
2018-11-14 13:40:02 +0000

    💠🏏 Checkpoint ./src/backend/server.js:728660/563 ./src/backend/utils.js:728660/653 ./patches/cpuCount.js:728660/131 ./tools/watch.sh:728660/239 ./watch.json:728660/22

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,38 +1,22 @@
 const fs = require("fs");
 
 const express = require("express");
-const proxy = require('http-proxy-middleware');
 
-const {wrap} = require("./utils");
+const {wrap, pause, proxyHMR} = require("./utils");
 
 const app = express();
-
-const hmrProxy = proxy({
-  target: 'ws://localhost:12345/',
-  ws: true,
-  ignorePath: true,
-  onOpen: (socket) => {
-    // keepalive
-    const handler = setInterval(() => {
-      socket.write('\x89\x00', 'binary');
-    }, 30000);
-    socket.on('close', () => {
-      clearInterval(handler);
-    });
-  },
-});
-app.use('/__hmr:12345/', hmrProxy);
+const proxy = proxyHMR(app);
 
 app.use(express.json());
 
 app.use(express.static("dist/"));
 
 app.get("*", wrap(async (req, res) => {
-  if (!fs.existsSync("/app/dist/index.html")) {
-    res.end("Please wait for the build to finish");
+  while (!fs.existsSync("/app/dist/index.html")) {
+    await pause(500);
   }
   res.sendFile("/app/dist/index.html");
 }));
 
 const listener = app.listen(process.env.PORT);
-listener.on('upgrade', hmrProxy.upgrade);
+listener.on('upgrade', proxy.upgrade);
--- src/backend/utils.js
+++ src/backend/utils.js
@@ -1,5 +1,7 @@
+const proxy = require("http-proxy-middleware");
+
 module.exports = self = {
-  wrap (fn) {
+  wrap(fn) {
     return async (req, res, next) => {
       try {
         await fn(req, res, next);
@@ -7,5 +9,30 @@ module.exports = self = {
         next(err);
       }
     };
-  }
+  },
+  
+  pause(ms) {
+    return new Promise((resolve) =>  {
+      setTimeout(resolve, ms);
+    });
+  },
+  
+  proxyHMR (app) {
+    const hmrProxy = proxy({
+      target: 'ws://localhost:12345/',
+      ws: true,
+      ignorePath: true,
+      onOpen: (socket) => {
+        // keepalive
+        const handler = setInterval(() => {
+          socket.write('\x89\x00', 'binary');
+        }, 30000);
+        socket.on('close', () => {
+          clearInterval(handler);
+        });
+      },
+    });
+    app.use('/__hmr:12345/', hmrProxy);
+    return hmrProxy;
+  },
 }
