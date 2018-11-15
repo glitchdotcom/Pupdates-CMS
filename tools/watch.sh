45efe57bf644004241db059f64fc32a60e4de96e
Glitch (parcel-test) <none>
2018-11-15 12:55:59 +0000

    🌠🏫 Checkpoint ./tools/watch.sh:728660/4

--- tools/watch.sh
+++ tools/watch.sh
@@ -3,7 +3,7 @@ set -e
 
 export TMP=/app/.tmp
 # We set max old space size to a very small number because parcel uses multiple processes
-export NODE_OPTIONS="--max-old-space-size=256"
+export NODE_OPTIONS="--max-old-space-size=200"
 
 echo "Wait for first build..."
 parcel watch --no-autoinstall --hmr-port 12345 --hmr-hostname $PROJECT_DOMAIN.glitch.me/__hmr \
