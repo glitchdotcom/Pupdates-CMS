2baac4159c1870acd0e193bb1d3b1c591daa0776
Glitch (parcel) <none>
2018-11-28 11:40:09 +0000

    🐙👏 Checkpoint ./backend/utils.js:728660/21

--- backend/utils.js
+++ backend/utils.js
@@ -18,7 +18,7 @@ module.exports = self = {
     });
   },
   
-  // This is needed to make Parcel Hot Module Reload (live updates) work on Glitch.
+  // This is needed to make Parcel Hot Module Replacement (live-updates) work on Glitch.
   proxyParcelHMR (app) {
     const hmrProxy = proxy({
       target: 'ws://localhost:12345/',
