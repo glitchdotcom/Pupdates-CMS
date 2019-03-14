5daecbb3eeee888e7f20dbd522c0ff7b9d50b02d
Glitch (parcel) <none>
2019-03-14 14:20:04 +0000

    🐆🎮 Checkpoint ./package.json:271885/4 ./frontend/index.js:271885/1064

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import CircularIndicator from './components/circular-indicator';
 import SimpleButton from './components/simple-button';
 
-const test = (
-  <div>
-    <CircularIndicator value={40} size={200} thickness={10}/>
-    <SimpleButton onClick={() => {alert("Hello!");}}>Click me!</SimpleButton>
-  </div>
-);
+const Main = () => {
+  const [value, setValue] = useState(40);
+  const inc = () => setValue(Math.min(value + 10, 100));
+  const dec = () => setValue(Math.max(0, value - 10));
+  return (
+    <div>
+      <CircularIndicator value={value} size={200} thickness={10} />
+      <SimpleButton onClick={inc}>More!</SimpleButton>
+      <SimpleButton onClick={dec}>Less!</SimpleButton>
+    </div>
+  );
+};
 
-ReactDOM.render(test, document.querySelector('#app'));
+ReactDOM.render(<Main />, document.querySelector('#app'));
