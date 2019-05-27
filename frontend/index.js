ae269620ff3199b27b2b3479f0bfd858f96218c2
Glitch (parcel) <none>
2019-05-27 18:08:26 +0000

    😅🐷 Checkpoint ./frontend/index.js:271885/1522

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
 
-import CircularIndicator from './components/circular-indicator';
-import SimpleButton from './components/simple-button';
+const CurrentUserContext = createContext()
+const useCurrentUser = () => useContext(CurrentUserContext)
+
+const LoggedIn = ({ children}) => {
+  const [currentUserFromLocalStorage, setCurrentUserFromLocalStorage] = useLocalStorage('currentUser')
+  const [currentUser, setCurrentUser] = useState(currentUserFromLocalStorage)
+  useEffect(() => {
+    if (currentUser) return
+    let shouldUseFetch = true
+    fetch()
+    return () => { shouldUseFetch = false }
+  }, [])
+  
+  return (
+    <CurrentUserContext.Provider>{children}</CurrentUserContext.Provider>
+  )
+}
+
+
+
 
 const Main = () => {
-  const [value, setValue] = useState(40);
-  const inc = () => setValue(Math.min(value + 10, 100));
-  const dec = () => setValue(Math.max(0, value - 10));
   return (
     <div>
-      <CircularIndicator value={value} size={200} thickness={10} />
-      <SimpleButton onClick={inc}>More!</SimpleButton>
-      <SimpleButton onClick={dec}>Less!</SimpleButton>
+      <div>Hello, world!</div>     
     </div>
   );
 };
