a7105a0989a11d163b8deed7640cab7eeb53e882
Glitch (parcel) <none>
2019-05-28 00:47:36 +0000

    👮🐬 Checkpoint ./frontend/index.js:271885/5346

--- frontend/index.js
+++ frontend/index.js
@@ -5,11 +5,33 @@ import { configureStore, createSlice } from 'redux-starter-kit'
 import { Provider, useDispatch, useSelector } from 'react-redux'
 import { matchTypes, after } from 'redux-aop'
 
+// localStorage
+
+function getFromStorage (key, defaultValue) {
+  const storedValue = localStorage.getItem(key)
+  if (storedValue) return JSON.parse(storedValue)
+  return defaultValue
+}
+
+function setStorage (key, newValue) {
+  localStorage.setItem(key, JSON.stringify(newValue))
+}
+
+function clearStorage (key) {
+  localStorage.removeItem(key)
+}
+
+// app
+
+const API_URL = 'https://api.glitch.com'
+
 const app = {
   actions: {
     mounted: () => ({ type: 'app/mounted' })
   }
 }
+
+// current user
 
 const currentUser = createSlice({
   slice: 'currentUser',
@@ -28,7 +50,13 @@ const currentUser = createSlice({
       status: 'loggedOut',
       currentUser: null,
     }),
-    loggedIn: (state) => ({ ...state, status: 'loading' })
+    submittedEmail: (state) => state,
+    submittedSignInCode: (state) => ({ ...state, status: 'loading' }),
+    loggedOut: (state) => ({ 
+      ...state, 
+      status: 'loggedOut',
+      currentUser: null,
+    }),
   },
 })
 
@@ -36,32 +64,38 @@ currentUser.middleware = [
   after(matchTypes('app/mounted'), async (store) => {
     const persistentToken = getFromStorage('persistentToken')
     try {
-      const currentUser = getUserForPersistentToken(persistentToken)
+      const currentUser = await getUserForPersistentToken(persistentToken)
       store.dispatch(currentUser.actions.loadedLoggedInUser(currentUser))
     } catch (e) {
       store.dispatch(currentUser.actions.loadedLoggedOutUser())
     }
   }),
-  after(matchTypes(currentUser.actions.loggedIn), (store, { payload: persistentToken }) => {
-    
-  })
+  after(matchTypes(currentUser.actions.submittedEmail), (_, { payload: emailAddress }) => {
+    fetch(`${API_URL}/email/sendLoginEmail`, { 
+      method: 'POST', 
+      body: JSON.stringify({ emailAddress }),
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        // TODO: need auth header here?
+      },
+    })
+  }),
+  after(matchTypes(currentUser.actions.submittedSignInCode), async (store, { payload: code }) => {
+    try {
+      const res = await fetch(`${API_URL}/auth/email/${code}`, { method: 'POST', mode: 'cors' })
+      const { persistentToken } = await res.json()
+      const currentUser = await getUserForPersistentToken(persistentToken)
+      setStorage('persistentToken', persistentToken)
+      store.dispatch(currentUser.actions.loadedLoggedInUser(currentUser))
+    } catch (e) {
+      store.dispatch(currentUser.actions.loadedLoggedOutUser())
+    }
+  }),
+  after(matchTypes(currentUser.actions.loggedOut), async () => {
+    clearStorage('persistentToken')
+  }),
 ]
-
-const useLoggedInStatus = () => useSelector(store => store.currentUser.status)
-
-
-
-const store = configureStore({
-  reducers: {
-    currentUser: currentUser.reducer,
-  },
-  middleware: [...currentUser.middleware],
-})
-
-const CurrentUserContext = createContext()
-const useCurrentUser = () => useContext(CurrentUserContext)
-
-const API_URL = 'https://api.glitch.com'
 
 async function getUserForPersistentToken (persistentToken) {
   if (!persistentToken) throw new Error("No token provided")
@@ -74,15 +108,19 @@ async function getUserForPersistentToken (persistentToken) {
   }
 }
 
-function getFromStorage (key, defaultValue) {
-  const storedValue = localStorage.getItem(key)
-  if (storedValue) return JSON.parse(storedValue)
-  return defaultValue
-}
+const useLoggedInStatus = () => useSelector(store => store.currentUser.status)
 
-function setStorage (key, newValue) {
-  localStorage.setItem(key, JSON.stringify(newValue))
-}
+
+
+const store = configureStore({
+  reducers: {
+    currentUser: currentUser.reducer,
+  },
+  middleware: [...currentUser.middleware],
+})
+
+const useCurrentUser = () => useSelector(store => store.cur)
+
 
 function useAsyncFunction (fn, ...deps) {
   const [state, setState] = useState({ status: 'pending' })
@@ -142,50 +180,18 @@ const CodeForm = ({ onSubmit }) => {
 }
 
 const Login = () => {
-  const [status, setStatus] = useState('init') // init | submittedEmail | submittedSignInCode | error
-  const [, setPersistentToken] = useLocalStorage('persistentToken')
-  const submitEmail = (emailAddress) => {
+  const [status, setStatus] = useState('init') // init | submittedEmail
+  const dispatch = useDispatch()
+  const submitEmail = (email) => {
     setStatus('submittedEmail')
-    fetch(`${API_URL}/email/sendLoginEmail`, { 
-      method: 'POST', 
-      body: JSON.stringify({ emailAddress }),
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        // TODO: need auth header here?
-      },
-    })
+    dispatch(currentUser.actions.submittedEmail(email))
   }
   const submitSigninCode = async (code) => {
-    setStatus('submittedSignInCode')
-    const res = await fetch(`${API_URL}/auth/email/${code}`, { method: 'POST', mode: 'cors' })
-    if (!res.ok) {
-      setStatus('error')
-      return
-    }
-    const { persistentToken } = await res.json()
-    setPersistentToken(persistentToken)
-    actions.dispatch(currentUser.loggedIn)
-    window.location.reload()
+    dispatch(currentUser.actions.submittedSignInCode(code))
   }
   
   if (status === 'init') return <EmailForm onSubmit={submitEmail} />
-  if (status === 'submittedEmail') return <CodeForm onSubmit={submitSigninCode} />
-  return <Loading />
-}
-
-const CurrentUserController = ({ children }) => {
-  const [persistentToken] = useLocalStorage('persistentToken')
-  
-  const { status, value: currentUser } = useAsyncFunction(getUserForPersistentToken, persistentToken)
-  
-  return (
-    <CurrentUserContext.Provider value={currentUser}>
-      {status === 'pending' && <Loading />}
-      {status === 'resolved' && children}
-      {status === 'rejected' && <Login />}
-    </CurrentUserContext.Provider>
-  )
+  return <CodeForm onSubmit={submitSigninCode} />
 }
 
 const useAPI = () => {
