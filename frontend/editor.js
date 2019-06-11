92cc0283908d107c55100393e3b23cb44e168c11
Glitch (parcel) <none>
2019-06-11 19:39:07 +0000

    😚🤗 Checkpoint ./frontend/editor.js:271885/536

--- frontend/editor.js
+++ frontend/editor.js
@@ -20,6 +20,18 @@ const featureCalloutsReducer = createReducer([], {
   }
 })
 
+const sizes = ['inherit', '3rem', '2rem', '1.5rem', '1rem', '0.8rem']
+
+const Text = styled.div`
+  font-size: ${({ size }) => sizes[size || 0]};
+  font-weight: #{({ weight }) => weight || 'inherit'};
+`
+
+const SectionTitle = ({ children }) => (
+  <Box p></Box>
+)
+
+
 const SectionTitle = styled.h1`
   font-size: 2rem;
   font-weight: bold;
