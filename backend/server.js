29d098d61f38d1a9b1158ffad72fd8ff26c9ef95
Glitch (parcel) <none>
2019-06-21 16:20:19 +0000

    🍁✍️ Checkpoint ./backend/server.js:271885/23

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.static("dist"));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
